217df5c5100b32e95d11e9b6c2a2992d42cf3e5d
mitulpatel888 <mitulpatel888@gmail.com>
2017-04-15 19:27:33 -0400

    Use of replace and Append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,5 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
- var awesomeThougths ="I am Mitul and I am AWESOME!";
- console.log(awesomeThougths);
- var funThoughts = awesomeThougths.replace("AWESOME!","FUN");
-$("#main").append(funThoughts);
+This is empty on purpose! Your code to build the resume will go here.*/
+var name = "Mitul Patel";
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").append(formattedName);
