b0c132020af22bd2d061b62e419cf9c6843fabbf
mitulpatel888 <mitulpatel888@gmail.com>
2017-04-15 19:36:17 -0400

    Formatted Name and Role using Append as well as test prepend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.*/
 var name = "Mitul Patel";
+var role = "Web Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
+
 $("#header").append(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedRole);
