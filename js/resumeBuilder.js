a0711433f034bb16ba828455780a49402f177178
mitulpatel888 <mitulpatel888@gmail.com>
2017-04-15 18:28:36 -0400

    FUN!

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,4 +3,5 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  var awesomeThougths ="I am Mitul and I am AWESOME!";
  console.log(awesomeThougths);
- 
+ var funThoughts = awesomeThougths.replace("AWESOME!","FUN");
+$("#main").append(funThoughts);
