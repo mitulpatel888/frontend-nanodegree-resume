4f47de801599a38f066207b12cb6159470330b0c
mitulpatel888 <mitulpatel888@gmail.com>
2017-04-21 19:37:08 -0400

    Biography Quiz: Add BIO info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,5 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.*/
+This is empty on purpose! Your code to build the resume will go here.
 var name = "Mitul Patel";
 var role = "Web Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
@@ -7,3 +7,12 @@ var formattedName = HTMLheaderName.replace("%data%", name);
 $("#header").append(formattedName);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").append(formattedRole);
+*/
+var skills = ["HTML","CSS"];
+var bio = {
+	"name":"Mitul",
+	"role":"Web Developer",
+	"contact info":"XYZ@example.com", 
+	"Skills": skills,
+};
+$("#main").append(bio.name);
