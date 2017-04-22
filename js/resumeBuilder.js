861177e46ebdd754f65f35524ce1750ef8faa02b
mitulpatel888 <mitulpatel888@gmail.com>
2017-04-21 20:46:59 -0400

    Practice with Pbjects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,6 +8,7 @@ $("#header").append(formattedName);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").append(formattedRole);
 */
+/*
 var skills = ["HTML","CSS"];
 var bio = {
 	"name":"Mitul",
@@ -16,3 +17,24 @@ var bio = {
 	"Skills": skills,
 };
 $("#main").prepend(bio.name);
+bio.city = "Mumbai";
+bio.email = "test@example.com"; 
+bio["currentCity"] = "Ahmedabad"; 
+$("#main").append(bio.city);
+$("#main").append(bio["currentCity"]);
+*/
+var work = {
+	
+};
+	work.position ="IT Analyst";
+	work.Employer= "Microsoft";
+	work.yearsWorked = 3;
+	work.City = "New York City";
+var education = {
+	
+};
+	education["Name"] ="Guj. Tech. University";
+	education["Years"] = 4;
+	education["City"]= "Ahmedabad";
+$("#main").append(work["position"]);
+$("#main").append(education.Name);
