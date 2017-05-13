a278d8a9f5e9970de510639229b76d9b40f46a0d
mitulpatel888 <mitulpatel888@gmail.com>
2017-05-12 20:09:38 -0400

    Projects section added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,10 +11,11 @@ $("#header").append(formattedRole);
 
 
 var bio =  {
- 	"name": "Mitul",
+ 	"name": "Mitul Patel",
  	"role": "Web Developer",
  	"contact info": "XYZ@example.com",
- 	"skills": ["HTML", "CSS", "JavaScript"]
+ 	"skills": ["HTML", "CSS", "JavaScript"],
+	"github":"https://github.com/mitulpatel888/frontend-nanodegree-resume"
  };
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 $("#header").append(formattedName);
@@ -27,47 +28,100 @@ bio.Pic = "images/fry.jpg"
 $("#main").append(bio["currentCity"]); */
 
 var work = {
+	"jobs": [
+	{
+		"title" :"IT Analyst",
+		"employer": "Company 1",
+		"yearsWorked": 3,
+		"location": "New York City",
+		"dates": "Jan-10 to Dec-13",
+		"description":"Analyst"
+	},	
+	{
+		"title" :"Web Developer",
+		"employer": "Microsoft",
+		"yearsWorked": 3,
+		"location": "New York City",
+		"dates": "Jan-14 to Dec-16",
+		"description":"Developer"
+	}
+
+	]
+}
+function displayWork(){
 	
-};
-	work.position ="IT Analyst";
+	for (job in work.jobs){
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+		
+	}
+	
+}
+	/* work.position ="IT Analyst";
 	work.Employer= "Microsoft";
 	work.yearsWorked = 3;
-	work.City = "New York City";
-var education = {
-	
-};
-	education["Name"] ="Guj. Tech. University";
-	education["Years"] = 4;
-	education["City"]= "Ahmedabad";
+	work.City = "New York City"; */
+
 /* $("#main").append(work["position"]);
 $("#main").append(education.Name); */
-var project = {
+
+var projects = {
 	"projects": [
 		{
 			"title":"Online Resume",
-			"year": 2017, 
-			"type": "Online"
+			"dates": "January-2017 to Match-2017", 
+			"description" : "Udacity FEND",
+			"type": "Online",
+			"images": "http://2.bp.blogspot.com/-ny_Rxx2XajM/UJATUm9h8mI/AAAAAAAABuI/DXM0WkmwwCg/s1600/flat_cv.png"
 		},
 		{
 			"title":"Sample2",
-			"year":"2017",
-			"type":"online"
+			"dates": "January-2017 to Match-2017",
+			"description" : "Test Sample",
+			"type":"online",
+			"image": "C:\Users\MPATEL02\Documents\Udacity\Portfolio\Online Resume\frontend-nanodegree-resume\images\fry.jpeg"
 		}
 	]
 }
+function displayProject(){
+	for (project in projects.projects){
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+		var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+		if (projects.projects[project].images.length>0){
+			for (image in projects.projects[project].images){
+				var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+				
+			}
+		}
+	}
+	
+}
+
 var education = {
 	"schools": [
 		{
 			"name": "GTU",
-			"city": "Ahmedabad",
-			"Degree": "ECE",
-			"Graduation": 2014
+			"location": "Ahmedabad",
+			"degree": "B.E",
+			"major": "ECE",
+			"graduation": 2014,
+			"dates":"July2010 to July-2014"
+			
 		},
 		{
 			"name": "A.G High School",
 			"city": "Ahmedabad",
-			"Degree": "High School",
-			"Graduation": 2010
+			"degree": "High School",
+			"graduation": 2010
 		}
 	],
 	"onlineCourse": {
@@ -87,4 +141,38 @@ if(bio.skills.length>0){
 	
 	
 }
+
+/* work.jobs.forEach(function(job)
+	{
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+		var formattedDates = HTMLworkDates.replace("%data",work.jobs[job].dates);
+		$(."work-entry:last").append(formattedDates);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
+}); */
+
+$(document).click(function(loc) {
+  var x= loc.pageX;
+  var y=loc.pageY;
+  logClicks(x,y);
+  });
+function inName(name){
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase + name[0].toLowerCase();
+	
+	return name[0]+" "+name[1];
+}
+displayProject();
+displayWork();
+$("#main").append(internationalizeButton);
+
+$("#mapDiv").append(googleMap); 
+
 
